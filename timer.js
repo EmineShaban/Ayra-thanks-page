4267d1a49e3723692baa91a88b3bfc124f73faf8
Emine Shaban <emineshaban@icloud.com>
2024-07-03 09:33:24 +0300

    .

---
+++ timer.js
@@ -0,0 +1,52 @@
+let minutesContainer = document.querySelectorAll("[id='minutes']");
+let secondsContainer = document.querySelectorAll("[id='seconds']");
+let piecesContainer = document.querySelectorAll("[id='pieces']");
+
+// function
+function countDownFunc() {
+    const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+    let currentDate = new Date();
+    let saved_countdown = localStorage.getItem('saved_countdown');
+
+    let futureDate = new Date(currentDate.getTime() + minute * 3);
+
+    if (saved_countdown == null) {
+        localStorage.setItem('saved_countdown', futureDate);
+    } else {
+        futureDate = saved_countdown;
+    }
+
+    // timer
+    (countDown = new Date(futureDate).getTime()),
+    (x = setInterval(function () {
+        let now = new Date().getTime(),
+            distance = countDown - now;
+        // for (let i = 0; i < hoursContainer.length; i++) {
+        //     let hours = (hoursContainer[i].innerText = Math.floor(
+        //         (distance % day) / hour
+        //     ));
+        // }
+
+        for (let i = 0; i < minutesContainer.length; i++) {
+            let minutes = (minutesContainer[i].innerText = Math.floor(
+                (distance % hour) / minute
+            ));
+        }
+
+        for (let i = 0; i < secondsContainer.length; i++) {
+            let seconds = (secondsContainer[i].innerText = Math.floor(
+                (distance % minute) / second
+            ));
+        }
+
+        if (distance <= 0) {
+            localStorage.removeItem('saved_countdown');
+            countDownFunc();
+        }
+    }, 0));
+}
+
+countDownFunc();
